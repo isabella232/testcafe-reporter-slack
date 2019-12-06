6917dd680231866e6540d2869325c9b18409c215
Juan Pablo Mansor <jmansor@confluent.io>
2019-12-06 12:57:40 -0300

    Fixes formatted test report errors code blocks in slack

--- src/SlackMessage.js
+++ src/SlackMessage.js
@@ -1,9 +1,9 @@
-import config from './config';
+import config from "./config";
 import loggingLevels from "./const/LoggingLevels";
 
 export default class SlackMessage {
   constructor() {
-    let slackNode = require('slack-node');
+    let slackNode = require("slack-node");
     this.slack = new slackNode();
     this.slack.setWebhook(config.webhookUrl);
     this.loggingLevel = config.loggingLevel;
@@ -12,49 +12,68 @@ export default class SlackMessage {
   }
 
   addMessage(message) {
-    this.messages.push(message)
+    this.messages.push(message);
   }
 
   addErrorMessage(message) {
-    this.errorMessages.push(message)
+    this.errorMessages.push(message);
   }
 
   sendMessage(message, slackProperties = null) {
-    this.slack.webhook(Object.assign({
-      channel: config.channel,
-      username: config.username,
-      text: message
-    }, slackProperties), function (err, response) {
-      if (!config.quietMode) {
-        if (err) {
-          console.log('Unable to send a message to slack');
-          console.log(response);
-        } else {
-          console.log(`The following message is send to slack: \n ${message}`);
+    this.slack.webhook(
+      Object.assign(
+        {
+          channel: config.channel,
+          username: config.username,
+          ...message
+        },
+        slackProperties
+      ),
+      function(err, response) {
+        if (!config.quietMode) {
+          if (err) {
+            console.log("Unable to send a message to slack");
+            console.log(response);
+          } else {
+            console.log(
+              `The following message is send to slack: \n ${message}`
+            );
+          }
         }
       }
-    })
+    );
   }
 
   sendTestReport(nrFailedTests) {
-    this.sendMessage(this.getTestReportMessage(), nrFailedTests > 0 && this.loggingLevel === loggingLevels.TEST
-      ? {
-        "attachments": [{
-          color: 'danger',
-          text: `${nrFailedTests} test failed`
-        }]
-      }
-      : null
-    )
+    this.sendMessage(
+      this.getTestReportMessage(),
+      nrFailedTests > 0 && this.loggingLevel === loggingLevels.TEST
+        ? {
+            attachments: [
+              {
+                color: "danger",
+                text: `${nrFailedTests} test failed`
+              }
+            ]
+          }
+        : null
+    );
   }
 
   getTestReportMessage() {
-    let message = this.getSlackMessage();
+    let message = { text: this.getSlackMessage(), blocks: [] };
     let errorMessage = this.getErrorMessage();
 
     if (errorMessage.length > 0 && this.loggingLevel === loggingLevels.TEST) {
-      message = message + "\n\n\n```" + this.getErrorMessage() + '```';
+      message.blocks.push({
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: "\n\n\n```" + this.getErrorMessage() + "```"
+        }
+      });
     }
+
     return message;
   }
 
