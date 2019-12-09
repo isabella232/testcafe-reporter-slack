734d9bc5913749348c1f449997b81c00abddeeb6
Juan Pablo Mansor <jmansor@confluent.io>
2019-12-09 11:24:57 -0300

    Adds new build to include slack webhooks parameter changes.

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,42 +1,55 @@
-const gulp = require('gulp');
-const babel = require('gulp-babel');
-const mocha = require('gulp-mocha');
-const del = require('del');
+const gulp = require("gulp");
+const babel = require("gulp-babel");
+const mocha = require("gulp-mocha");
+const del = require("del");
 
-gulp.task('clean', function (cb) {
-  del('lib', cb);
+gulp.task("clean", function(cb) {
+  del("lib", cb);
 });
 
-gulp.task('build', ['clean'], function () {
-  return gulp
-    .src('src/**/*.js')
-    .pipe(babel())
-    .pipe(gulp.dest('lib'));
-});
+gulp.task(
+  "build",
+  gulp.parallel("clean", function() {
+    return gulp
+      .src("src/**/*.js")
+      .pipe(babel())
+      .pipe(gulp.dest("lib"));
+  })
+);
 
-gulp.task('watch', ['build'], function () {
-  gulp.watch('src/*.js', ['build']);
-});
+gulp.task(
+  "watch",
+  gulp.parallel("build", function() {
+    gulp.watch("src/*.js", ["build"]);
+  })
+);
 
-gulp.task('test', ['build'], function () {
-  return gulp
-    .src('test/**.js')
-    .pipe(mocha({
-      ui: 'bdd',
-      reporter: 'spec',
-      timeout: typeof v8debug === 'undefined' ? 2000 : Infinity // NOTE: disable timeouts in debug
-    }));
-});
+gulp.task(
+  "test",
+  gulp.parallel("build", function() {
+    return gulp.src("test/**.js").pipe(
+      mocha({
+        ui: "bdd",
+        reporter: "spec",
+        timeout: typeof v8debug === "undefined" ? 2000 : Infinity // NOTE: disable timeouts in debug
+      })
+    );
+  })
+);
 
-gulp.task('preview', ['build'], function () {
-  const buildReporterPlugin = require('testcafe').embeddingUtils.buildReporterPlugin;
-  const pluginFactory = require('./lib');
-  const reporterTestCalls = require('./test/utils/reporter-test-calls');
-  const plugin = buildReporterPlugin(pluginFactory);
+gulp.task(
+  "preview",
+  gulp.parallel("build", function() {
+    const buildReporterPlugin = require("testcafe").embeddingUtils
+      .buildReporterPlugin;
+    const pluginFactory = require("./lib");
+    const reporterTestCalls = require("./test/utils/reporter-test-calls");
+    const plugin = buildReporterPlugin(pluginFactory);
 
-  reporterTestCalls.forEach(function (call) {
-    plugin[call.method].apply(plugin, call.args);
-  });
+    reporterTestCalls.forEach(function(call) {
+      plugin[call.method].apply(plugin, call.args);
+    });
 
-  process.exit(0);
-});
+    process.exit(0);
+  })
+);
